f8e478d41eee3f91411b7c8864c6902187d8cd50
RicardoEPaez <ricardo.paez@correo.unimet.edu.ve>
2024-10-27 14:05:31 -0400

    Puse coments que hacen cada parte del codigo

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const txtOp1 = document.getElementById("op1");
-const txtOperacion = document.getElementById("operacion");
-const txtOp2 = document.getElementById("op2");
-const btnCalcular = document.getElementById("calcular");
-const pResultado = document.getElementById("resultado");
+const txtOp1 = document.getElementById("op1"); //  Recupera el primer elemento de entrada de operando por su identificador "op1".
+const txtOperacion = document.getElementById("operacion"); //Recupera el elemento de entrada de operación por su identificador "operacion".
+const txtOp2 = document.getElementById("op2"); //Recupera el segundo elemento de entrada de operando por su identificador "op2".
+const btnCalcular = document.getElementById("calcular"); //Recupera el elemento de botón "calcular" por su identificador.
+const pResultado = document.getElementById("resultado"); //Recupera el elemento de párrafo "resultado" por su identificador.
 
-btnCalcular.addEventListener('click', calcular);
+btnCalcular.addEventListener('click', calcular); //Asigna un controlador de eventos de clic al botón "calcular", lo que desencadena la función calcular al hacer clic.
 
-function calcular() {
+function calcular() { //Define la funcion calcular que realiza calculo segun la operacion deseada
     const operacion = txtOperacion.value;
     const op1 = parseFloat(txtOp1.value);
     const op2 = parseFloat(txtOp2.value);
